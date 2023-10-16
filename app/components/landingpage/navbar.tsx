0fc03faf4e8d2c7a4c224dae397e9744b5214bb9
Mutangana Edgar <edgar.mutangana@amalitech.com>
2023-10-16 22:32:54 +0200

    background colors and navbar

---
+++ app/components/landingpage/navbar.tsx
@@ -0,0 +1,31 @@
+export default function NavBar() {
+    return(
+        <>
+            <nav className="w-[80%] h-[100px] border-b border-[#616161] text-black flex m-auto justify-evenly items-center">
+                <ul>
+                    <li>
+                        <h1 className="font-sans text-2xl text-left"><span className="text-cyan-800 font-bold bg-black p-2">LittleLives</span><span className="text-black-800 font-bold bg-white p-2">Consult</span></h1>
+                    </li>
+                </ul>
+                <ul className="flex justify-evenly items-ceter">
+                    <li>
+                        <a className="" href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About us</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li><a href="#">(+62)81 414 257</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 /* Components */
 import { Providers } from '@/lib/providers'
-import { Nav } from './components/Nav'
-
 /* Instruments */
 import styles from './styles/layout.module.css'
 import './styles/globals.css'
+import NavBar from './components/landingpage/navbar'
 
 export default function RootLayout(props: React.PropsWithChildren) {
   return (
@@ -12,52 +11,11 @@ export default function RootLayout(props: React.PropsWithChildren) {
       <html lang="en">
         <body>
           <section className={styles.container}>
-            <Nav />
+            <NavBar />
 
-            <header className={styles.header}>
-              <img src="/logo.svg" className={styles.logo} alt="logo" />
-            </header>
+            <main></main>
 
-            <main className={styles.main}>{props.children}</main>
-
-            <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-            </footer>
+            <footer></footer>
           </section>
         </body>
       </html>
